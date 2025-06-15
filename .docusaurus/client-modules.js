34be9b648a105a88c0320a915e2698ec22723f50
Munim <munim@Munims-MacBook-Pro.local>
2025-06-16 01:04:51 +0500

    Deploy website - based on 00b98b6792dc15e74221d815230708774a860d1c

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/munim/maldives-guide/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/munim/maldives-guide/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/munim/maldives-guide/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/munim/maldives-guide/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/munim/maldives-guide/src/css/custom.css"),
-];
